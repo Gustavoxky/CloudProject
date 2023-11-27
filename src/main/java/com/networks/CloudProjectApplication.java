9665c57b3b396d9085d1f30d0940eb9f650603e9
Gustavoxky <gustavoxky@gmail.com>
2023-11-27 20:59:02 -0300

    update

--- src/main/java/com/networks/CloudProjectApplication.java
+++ src/main/java/com/networks/CloudProjectApplication.java
@@ -7,17 +7,33 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CloudProjectApplication {
 
 	public static void main(String[] args) {
-		CloudSimPlusExample1.run(args);
-		CloudSimPlusExample2.run(args);
-		CloudSimPlusExample3.run(args);
-		CloudSimPlusExample4.run(args);
-		CloudSimPlusExample5.run(args);
-		CloudSimPlusExample6.run(args);
-		CloudSimPlusExample7.run(args);
-		CloudSimPlusExample8.run(args);
-//		MultipleHostsAndVMsExample.run(args);
-		NetworkSimulationExample.run(args);
+		// Lista de exemplos do CloudSim Plus
+		Class<?>[] cloudSimPlusExamples = {
+				CloudSimPlusExample1.class,
+				CloudSimPlusExample2.class,
+				CloudSimPlusExample3.class,
+				CloudSimPlusExample4.class,
+				CloudSimPlusExample5.class,
+				CloudSimPlusExample6.class,
+				CloudSimPlusExample7.class,
+				CloudSimPlusExample8.class,
+				// Adicione mais exemplos conforme necessário
+				// MultipleHostsAndVMsExample.class,
+				NetworkSimulationExample.class
+		};
 
+		// Executando exemplos do CloudSim Plus
+		for (Class<?> exampleClass : cloudSimPlusExamples) {
+			runExample(exampleClass, args);
+		}
+	}
+
+	private static void runExample(Class<?> exampleClass, String[] args) {
+		try {
+			// Obter o método estático "run" da classe de exemplo e invocá-lo
+			exampleClass.getMethod("run", String[].class).invoke(null, (Object) args);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
-
